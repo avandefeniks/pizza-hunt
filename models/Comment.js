f04ec57945aa50c31e7509e4c6eb9a56624da4e7
avandefeniks <angel.vandefeniks@gmail.com>
2022-07-16 14:00:19 -0700

    validation added

--- models/Comment.js
+++ models/Comment.js
@@ -1,3 +1,4 @@
+const { truncate } = require('fs');
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
@@ -9,10 +10,14 @@ const ReplySchema = new Schema(
       default: () => new Types.ObjectId()
     },
     replyBody: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     writtenBy: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
@@ -30,10 +35,14 @@ const ReplySchema = new Schema(
 const CommentSchema = new Schema(
   {
     writtenBy: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     commentBody: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
